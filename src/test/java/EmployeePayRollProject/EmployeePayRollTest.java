cac2369046e26333c66478debead1890fcaebf52
madhu179 <madhubabu232343@gmail.com>
2020-11-02 15:50:06 +0530

    Adding code to update employee salary in json server

--- src/test/java/EmployeePayRollProject/EmployeePayRollTest.java
+++ src/test/java/EmployeePayRollProject/EmployeePayRollTest.java
@@ -329,5 +329,18 @@ public class EmployeePayRollTest {
 		countOfEntries = empPayRollService.addEmployeeAndPayRoll(employeeList,"REST_IO");
 		Assert.assertEquals(3, countOfEntries);
 	}
+	
+	@Test
+	public void givenNewSalary_WhenUpdated_ShouldReturnSucessCode(){
+		EmployeePayRoll employeePayRoll = new EmployeePayRoll(4, "Natasha", "F", 80000.0, 3, Arrays.asList("Management"),
+				Arrays.asList(LocalDate.parse("2020-04-29")));
+		String jsonString = new Gson().toJson(employeePayRoll);
+		RequestSpecification request = RestAssured.given();	
+		request.header("Content-Type","application/json");
+		request.body(jsonString);
+		Response  response = request.put("/employee_payroll/"+employeePayRoll.id);
+		int statusCode = response.getStatusCode();
+		Assert.assertEquals(200, statusCode);
+	}
 
 }
